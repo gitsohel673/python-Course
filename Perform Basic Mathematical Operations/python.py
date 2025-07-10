d00006f2fa5a5fa441c7d02363b08d657081e6a9
gitsohel673 <shahinmulla673@gmail.com>
2025-07-10 22:43:39 +0530

    Add files via upload

---
+++ Perform Basic Mathematical Operations/python.py
@@ -0,0 +1,10 @@
+num1 = input("Enter first number :")
+num2 = input("Enter second number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
