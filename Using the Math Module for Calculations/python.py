60bf5ed6fa66bedb54d578ffbd51f4d941beead7
gitsohel673 <shahinmulla673@gmail.com>
2025-07-13 00:01:19 +0530

    Add files via upload

---
+++ Using the Math Module for Calculations/python.py
@@ -0,0 +1,11 @@
+import math
+
+number = int(input("Enter a number :"))
+
+squareRoot = math.sqrt(number)
+Logarithm = math.log(number)
+sine = math.sin(number)
+
+print(squareRoot)
+print(Logarithm)
+print(sine)
